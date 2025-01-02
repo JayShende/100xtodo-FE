2d9b7d84b2b7bcaff05095cf90830a0c1da796be
JayShende <shendejay379@gmail.com>
2025-01-02 13:22:38 +0530

    fix in app.tsx as per error logs

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import LoginPage from './pages/login-page'
 import SignupPage from './pages/signup-page'
-import { Homepage } from './pages/home-page'
 import { DashBoard } from './pages/dashBoard-page'
+import { Homepage } from './pages/home-Page'
 
 function App() {
 
